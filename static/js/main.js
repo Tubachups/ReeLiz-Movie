373cc874301490017f17b2faef2242acc965a6cd
John Lloyd <johnlloydtubachups@gmail.com>
2025-09-17 13:38:55 +0800

    Separate dropdown logic to own file in main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,6 @@
-import { fetchGenres, fetchMovies } from './services/movieService.js';
-import { renderMovies } from './components/movieRenderer.js';
+import { fetchGenres, fetchMovies } from "./services/movieService.js";
+import { renderMovies } from "./components/movieRenderer.js";
+import { setupGenresDropdown } from "./components/dropdownHandler.js";
 
 let genresMap = {};
 let allMovies = [];
@@ -32,7 +33,11 @@ async function initializeGenres() {
     map[g.id] = g.name;
     return map;
   }, {});
-  setupGenresDropdown(genres);
+  setupGenresDropdown(
+    genres,
+    (genreId) => filterMoviesByGenre(genreId),
+    () => renderMovies(allMovies, genresMap)
+  );
 }
 
 async function loadMovies(type) {
@@ -47,62 +52,6 @@ function filterMoviesByGenre(genreId) {
     movie.genre_ids.includes(parseInt(genreId))
   );
   renderMovies(filtered, genresMap);
-}
-
-function setupGenresDropdown(genres) {
-  const genresDropdown = document.querySelector("#genres-dropdown");
-  if (!genresDropdown) return;
-
-  genresDropdown.classList.add('columns-2');
-  genresDropdown.innerHTML = '';
-
-  const gridContainer = document.createElement('div');
-  gridContainer.className = 'genres-grid';
-
-  addAllGenresOption(gridContainer);
-  addGenreOptions(gridContainer, genres);
-
-  genresDropdown.appendChild(gridContainer);
-}
-
-function addAllGenresOption(container) {
-  const allGenresLi = document.createElement('div');
-  const allGenresLink = document.createElement('a');
-  allGenresLink.className = 'dropdown-item';
-  allGenresLink.href = '#';
-  allGenresLink.innerText = 'All Genres';
-  allGenresLink.addEventListener('click', (e) => {
-    e.preventDefault();
-    renderMovies(allMovies, genresMap);
-    updateActiveGenre(allGenresLink);
-  });
-  allGenresLi.appendChild(allGenresLink);
-  container.appendChild(allGenresLi);
-}
-
-function addGenreOptions(container, genres) {
-  genres.forEach((genre) => {
-    const genreItem = document.createElement('div');
-    const genreLink = document.createElement('a');
-    genreLink.className = 'dropdown-item';
-    genreLink.href = '#';
-    genreLink.innerText = genre.name;
-
-    genreLink.addEventListener('click', (e) => {
-      e.preventDefault();
-      filterMoviesByGenre(genre.id);
-      updateActiveGenre(genreLink);
-    });
-
-    genreItem.appendChild(genreLink);
-    container.appendChild(genreItem);
-  });
-}
-
-function updateActiveGenre(activeLink) {
-  document.querySelectorAll('#genres-dropdown .dropdown-item')
-    .forEach(item => item.classList.remove('active'));
-  activeLink.classList.add('active');
 }
 
 init();
