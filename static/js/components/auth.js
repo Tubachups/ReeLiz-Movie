584b8b17b8bc70a961cab9df129e1233043a7f12
allyuh <mejiaallya00@gmail.com>
2025-10-23 19:59:19 +0800

    login changes

--- static/js/components/auth.js
+++ static/js/components/auth.js
@@ -1,5 +1,4 @@
-// auth.js - handles subtitle change between Login and Sign Up
-
+// Subtitle change
 document.addEventListener("DOMContentLoaded", () => {
   const subtitle = document.getElementById('tabSubtitle');
   const loginTab = document.getElementById('login-tab');
@@ -11,7 +10,32 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     signupTab.addEventListener('click', () => {
-      subtitle.textContent = 'Create your account';
+      subtitle.textContent = 'Create your account.';
     });
   }
+});
+
+// Transistion height
+const tabContent = document.getElementById('authTabsContent');
+const tabs = document.querySelectorAll('#authTabs button');
+const imageContainer = document.querySelector('.auth-image-container');
+
+function setTabHeight(activeTabId) {
+  const activePane = document.querySelector(activeTabId);
+  const height = activePane.scrollHeight;
+
+  tabContent.style.height = height + 'px';
+  imageContainer.style.height = height + 'px';
+}
+
+window.addEventListener('load', () => {
+  const activePane = document.querySelector('#authTabsContent .tab-pane.active');
+  setTabHeight('#' + activePane.id);
+});
+
+// Update height on tab show
+tabs.forEach(tab => {
+  tab.addEventListener('shown.bs.tab', e => {
+    setTabHeight(e.target.getAttribute('data-bs-target'));
+  });
 });
